707b54d43c28ddf1eef2c19987c722692c8cfdf1
Cal K <67473001+clkHere@users.noreply.github.com>
2024-02-04 21:32:53 -0500

    Update rock-paper-scissors.py

--- C. Projects/rock-paper-scissors.py
+++ C. Projects/rock-paper-scissors.py
@@ -1,70 +1,69 @@
-class Participant: # sets the actor blueprint
+class Participant:
     def __init__(self, name):
         self.name = name
         self.points = 0
         self.choice = ""
-
     def choose(self):
-        self.choice = input("{name}, select rock, paper, or scissor: ".format(name= self.name))
-        print("{name} selects {choice}".format(name=self.name,  choice = self.choice))
-
+        self.choice = input("{name}, select rock, paper or scissor: ".format(name=self.name))
+        print("{name} selects {choice}".format(name=self.name, choice=self.choice))
     def toNumericalChoice(self):
-        switcher = { "rock": 0, "paper": 1, "scissor": 2}
+        switcher = {
+            "rock": 0,
+            "paper": 1,
+            "scissor": 2
+        }
         return switcher[self.choice]
-
     def incrementPoint(self):
-        self.__points__ +=1 #Error on 02.04.2024 @ 15:19
+        self.points += 1
 
 class GameRound:
     def __init__(self, p1, p2):
-        self.rules = [ 
-            [0,-1,1], #Rock v [Rock, Paper, Scissors]
-            [1,0,-1], #Paper v [Rock, Paper, Scissors]
-            [-1,1,0] #Scissors v [Rock, Paper, Scissors]
-         ]
+        self.rules = [      # __Matrix array attribute of beatable combinations__
+            [0, -1, 1],     # Rock v [Rock, Paper, Scissors)
+            [1, 0, -1],     # Paper v [Rock, Paper, Scissors)
+            [-1, 1, 0]      # Scissors v [Rock, Paper, Scissors)
+        ]
         p1.choose()
         p2.choose()
-        result = self.compareChoices(p1,p2)
-        if result > 0: 
-            p1.incrementPoint() #Error on 02.04.2024 @ 15:19
-        elif result <0: 
+        result = self.compareChoices(p1, p2)
+        print("Round resulted in a {result}".format(result=self.getResultAsString(result)))
+        if result > 0:
+            p1.incrementPoint()
+        elif result < 0:
             p2.incrementPoint()
-        print("Round resulted in a {result}".format(result=self.getResultAsString(result) ))
-
+        else:
+            print("No points for anybody.")
     def compareChoices(self, p1, p2):
         return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
-
-    def getResultAsString(self,result):
-        res = {0: "draw", 1: "win", -1: "loss"}
-        return res[result]
-        
     def awardPoints(self):
         print("implement")
+    def getResultAsString(self, result):
+        res = {
+            0: "draw",
+            1: "win",
+            -1: "loss
+        }
+        return res[result]
 
-class Game: # Sets the game parameter blueprint
+class Game:
     def __init__(self):
-        self.endGame = False #sets the ending
-        self.participant = Participant("Spock") #sets the actor varible
-        self.secondParticipant = Participant("Kirk") #sets another actor variable
-
+        self.endGame = False
+        self.participant = Participant("Player 1")
+        self.secondParticipant = Participant("Player 2")
     def start(self):
         while not self.endGame:
-            GameRound(self.participant, self.secondParticipant) #Error on 02.04.2024 @ 15:19
+            GameRound(self.participant, self.secondParticipant)
             self.checkEndCondition()
-        
-    
     def checkEndCondition(self):
         answer = input("Continue game y/n: ")
         if answer == 'y':
             GameRound(self.participant, self.secondParticipant)
-            self.checkEndCondition
-        else: 
-            print("Game ended, {p1name} has {p1points}, and {p2name} has {p2points}".format(p1name = self.participant.name, p1points = self.participant.points, p2name = self.secondParticipant.name, p2points = self.secondParticipant.points))
+            self.checkEndCondition()
+        else:
+            print("Game ended, {p1name} has {p1points}, and {p2name} had {p2points}".format(p1name=self.participant.name, p1points=self.participant.points, p2name=self.secondParticipant.name, p2points=self.secondParticipant.points))
             self.determineWinner()
             self.endGame = True
-        
-
-    def determineWinner(Self):
+    def determineWinner(self):
         resultString = "It's a Draw"
         if self.participant.points > self.secondParticipant.points:
             resultString = "Winner is {name}".format(name=self.participant.name)
@@ -73,4 +72,4 @@ class Game: # Sets the game parameter blueprint
         print(resultString)
 
 game = Game()
-game.start() #Error on 02.04.2024 @ 15:19
+game.start()
